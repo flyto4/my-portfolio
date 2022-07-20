3b990a24b1c8653af93a9f9b4483a9497c1907c9
umararshad4 <mumararshad4444@gmail.com>
2022-07-20 07:57:28 +0500

    added the animation in the website

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import Alpine from "alpinejs";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// aos initialization
+AOS.init();
+
+// alpine initialization
 window.Alpine = Alpine;
 Alpine.start();
